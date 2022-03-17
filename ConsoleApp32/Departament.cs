617cc38848e4dbbc35bd801d57d47545f5459124
Lacromisss <tu7a45yc6@code.edu.az>
2022-03-18 02:36:44 +0400

    Add project files.

---
+++ ConsoleApp32/Departament.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    internal class Departament
+    {
+        private string _name;
+        private int employeLimit;
+        private int salaryLimit;
+        public string []employees;
+        public Employee [] students = new Employee[0];
+       
+
+
+
+        public void AddEmployee()
+        {
+            try
+            {
+                if (employees.Length < employeLimit)
+                {
+                    Array.Resize(ref employees, employees.Length + 1);
+
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("wrong");
+
+                throw;
+            }
+            
+          
+
+
+
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp32/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    internal class Employee
+    {
+        private string _name;
+        private string _surName;
+        private int _salary;
+        public string MyProperty
+        {
+            get => this._name;
+            set
+            {
+                if (nameSurnameControl(value))
+                {
+
+
+
+                    this._name = value;
+                }
+            }
+
+        }
+        public string MyProperty1 { get => this._surName;
+                set
+            {
+                if (nameSurnameControl(value))
+                {
+                    this._surName= value;
+                }
+            } 
+   
+        }
+        public int MyProperty2 { get=> this._salary;
+                set
+            {
+                if (value>250)
+                {
+                    this._salary = value;
+                }
+            }
+        }
+        public bool nameSurnameControl(string a)
+        {
+            if (string.IsNullOrWhiteSpace(a))
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (char.IsUpper(a[0]) && char.IsLetter(a[i]))
+                    {
+                        return true;
+
+
+                    }
+
+                }
+                
+
+            }
+            return false;
+
+
+
+
+
+        }
+        
+
+
+
+
+    }
+    }
+
